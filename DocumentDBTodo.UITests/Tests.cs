da3d98af93b01b665b89b8400b3322a853452a0e
samslee <samslee@MSHK12345678C>
2017-02-07 13:51:38 +0800

    Made changes to test.cs

--- DocumentDBTodo.UITests/Tests.cs
+++ DocumentDBTodo.UITests/Tests.cs
@@ -32,40 +32,48 @@ namespace DocumentDBTodo.UITests
 		}
 
 		[Test]
-		public void Login()
+		public void LoginWrong()
 		{
-			app.Tap(x => x.Marked("Input Your Username here"));
-            app.Screenshot("Tap on Username input field");
-            app.EnterText(x => x.Class("UITextField").Text("Samson"), " ");
-            app.Screenshot("Type Username");
-            app.PressEnter();
-            app.Screenshot("Done with Username");
-            app.Tap(x => x.Marked("Input Your Password here"));
-            app.Screenshot("Tap on Password input field");
-            app.EnterText(x => x.Class("UITextField").Index(1), "P@ssw0rd!");
-            app.Screenshot("Type Password");
-            app.PressEnter();
-            app.Screenshot("Done with Password");
+			//app.Tap(x => x.Marked("Input Your Username here"));
+            //app.EnterText(x => x.Class("UITextField").Text("Samson"), " ");
+            //app.PressEnter();
+            //app.Tap(x => x.Marked("Input Your Password here"));
+            //app.EnterText(x => x.Class("UITextField").Index(1), "P@ssw0rd!");
+            //app.PressEnter();
             app.Tap(x => x.Marked("Login"));
-			app.Screenshot("Tapped on Login");
+			app.Screenshot("Login with wrong credentials");
 		}
 
-		//[Test]
-		//public void DisplayErrorMessage_WrongPassword()
-		//{
-		//	app.Tap(x => x.Marked("OK"));
-		//	app.Screenshot("Tapped on view with class: _UIAlertControllerActionView marked: OK");
-		//}
+        public void LoginRight()
+        {
+            app.Tap(x => x.Marked("OK"));
+            app.Tap(x => x.Marked("Input Your Username here"));
+            app.EnterText(x => x.Class("UITextField"), "Samson");
+            app.PressEnter();
+            app.Tap(x => x.Marked("Input Your Password here"));
+            app.EnterText(x => x.Class("UITextField").Index(1), "P@ssw0rd");
+            app.PressEnter();
+            app.Tap(x => x.Marked("Login"));
+            app.Screenshot("Login with right credentials");
+        }
 
-		//[Test]
-		//public void InteractWithEventPage()
-		//{
-		//	app.ScrollUp();
-		//	app.Screenshot("Swipped down");
-		//	app.Tap(x => x.Text("Browse Events"));
-		//	app.Screenshot("Browse Events");
-		//	app.Tap(x => x.Marked("Back"));
-  //          app.Screenshot("Back to MainPage");
-  //      }
-	}
+
+        //[Test]
+        //public void DisplayErrorMessage_WrongPassword()
+        //{
+        //	app.Tap(x => x.Marked("OK"));
+        //	app.Screenshot("Tapped on view with class: _UIAlertControllerActionView marked: OK");
+        //}
+
+        //[Test]
+        //public void InteractWithEventPage()
+        //{
+        //	app.ScrollUp();
+        //	app.Screenshot("Swipped down");
+        //	app.Tap(x => x.Text("Browse Events"));
+        //	app.Screenshot("Browse Events");
+        //	app.Tap(x => x.Marked("Back"));
+        //  app.Screenshot("Back to MainPage");
+        //}
+    }
 }
